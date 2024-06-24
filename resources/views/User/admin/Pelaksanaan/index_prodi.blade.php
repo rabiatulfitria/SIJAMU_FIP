dc7a8fe4213800e1857205b7f7c4ae2209c52017
rabiatulfitria <fitriarobiatul13@gmail.com>
2024-06-24 11:24:17 +0700

    tabel tidak tampil

--- resources/views/User/admin/Pelaksanaan/index_prodi.blade.php
+++ resources/views/User/admin/Pelaksanaan/index_prodi.blade.php
@@ -68,17 +68,17 @@
 @endsection
 
 @section('content')
-    <div class="card">
+    <div class="">
         <div class="nav-align-top mb-4">
             <ul class="nav nav-tabs" role="tablist">
                 <li class="nav-item">
-                    <button type="button" class="nav-link active" role="tab" data-bs-toggle="tab"
+                    <button type="button" class="nav-link active" role="tab" data-bs-toggle="tab" href="{{ route('pelaksanaan.prodi') }}"
                         data-bs-target="#navs-top-home" aria-controls="navs-top-home" aria-selected="true">
                         Prodi
                     </button>
                 </li>
                 <li class="nav-item">
-                    <button type="button" class="nav-link" role="tab" data-bs-toggle="tab"
+                    <button type="button" class="nav-link" role="tab" data-bs-toggle="tab" href="{{ route('pelaksanaan.fotokegiatan') }}"
                         data-bs-target="#navs-top-profile" aria-controls="navs-top-profile" aria-selected="false">
                         Foto Kegiatan
                     </button>
@@ -86,37 +86,44 @@
             </ul>
             <div class="tab-content">
                 <div class="tab-pane fade show active" id="navs-top-home" role="tabpanel">
-                    <p>
-                        Icing pastry pudding oat cake. Lemon drops cotton candy caramels cake caramels sesame snaps
-                        powder. Bear claw candy topping.
-                    </p>
-                    <p class="mb-0">
-                        Tootsie roll fruitcake cookie. Dessert topping pie. Jujubes wafer carrot cake jelly. Bonbon
-                        jelly-o jelly-o ice cream jelly beans candy canes cake bonbon. Cookie jelly beans marshmallow
-                        jujubes sweet.
-                    </p>
+                    <div class="card mt-3">
+                        <div class="card-header">
+                            <h5>profil program studi</h5>
+                        </div>
+                        <div class="card-body">
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Program Studi</th>
+                                        <th>2020</th>
+                                        <th>2021</th>
+                                        <th>2022</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {{-- @foreach ($programStudis as $programStudi) --}}
+                                    {{-- <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $programStudi->name }} - {{ $programStudi->level }}</td>
+                                            <td>{{ $programStudi->akreditasi_2020 }}</td>
+                                            <td>{{ $programStudi->akreditasi_2021 }}</td>
+                                            <td>{{ $programStudi->akreditasi_2022 }}</td>
+                                        </tr> --}}
+                                    <tr>
+                                        <td>1</td>
+                                        <td>jsdbhdbnd xdsd djd xd</td>
+                                        <td>jsdbhdbnd</td>
+                                        <td>jsdbhdbnd</td>
+                                        <td>jsdbhdbnd</td>
+                                    </tr>
+                                    {{-- @endforeach --}}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
                 <div class="tab-pane fade" id="navs-top-profile" role="tabpanel">
-                    <p>
-                        Donut dragée jelly pie halvah. Danish gingerbread bonbon cookie wafer candy oat cake ice
-                        cream. Gummies halvah tootsie roll muffin biscuit icing dessert gingerbread. Pastry ice cream
-                        cheesecake fruitcake.
-                    </p>
-                    <p class="mb-0">
-                        Jelly-o jelly beans icing pastry cake cake lemon drops. Muffin muffin pie tiramisu halvah
-                        cotton candy liquorice caramels.
-                    </p>
-                </div>
-                <div class="tab-pane fade" id="navs-top-messages" role="tabpanel">
-                    <p>
-                        Oat cake chupa chups dragée donut toffee. Sweet cotton candy jelly beans macaroon gummies
-                        cupcake gummi bears cake chocolate.
-                    </p>
-                    <p class="mb-0">
-                        Cake chocolate bar cotton candy apple pie tootsie roll ice cream apple pie brownie cake. Sweet
-                        roll icing sesame snaps caramels danish toffee. Brownie biscuit dessert dessert. Pudding jelly
-                        jelly-o tart brownie jelly.
-                    </p>
                 </div>
             </div>
         </div>
