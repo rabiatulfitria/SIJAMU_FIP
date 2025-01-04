37ce5edd757a83b269cb984bcd8cdc11e7293d17
rabiatulfitria <fitriarobiatul13@gmail.com>
2025-01-04 09:55:44 +0700

    edit dan tambah tabel migrasi

--- database/migrations/2024_12_30_162751_create_tabel_prodi_table.php
+++ database/migrations/2024_12_30_162751_create_tabel_prodi_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('penetapans', function (Blueprint $table) {
-            $table->id('id_penetapan');
-            $table->string('tanggal_ditetapkan');
+        Schema::create('tabel_prodi', function (Blueprint $table) {
+            $table->id('id_prodi');
+            $table->string('nama_prodi');
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('penetapans');
+        Schema::dropIfExists('tabel_prodi');
     }
 };
